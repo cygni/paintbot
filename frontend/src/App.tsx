b9f2cf729931d2a7cb53a2e0dc98c071ac638b05
jeppzone <jeppzone@gmail.com>
2018-10-24 12:56:40 +0200

    Replace App.css with styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import './App.css';
 import GameContainer from './game/GameContainer';
 import * as background from './resources/background.jpg';
 
