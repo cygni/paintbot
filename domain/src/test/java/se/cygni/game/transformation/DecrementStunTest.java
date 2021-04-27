785219a72b9e996a34e3b41dc0e6685351d45296
Mårten Åsberg <marten.asberg@outlook.com>
2021-04-27 17:55:18 +0200

    Update WorldState using "with" methods
    Using the new method in `DecrementStun`, and testing it in `DecrementStunTest`.

--- domain/src/test/java/se/cygni/game/transformation/DecrementStunTest.java
+++ domain/src/test/java/se/cygni/game/transformation/DecrementStunTest.java
@@ -7,6 +7,10 @@ import se.cygni.game.worldobject.CharacterImpl;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Alan Tibbetts
  * @since 12/04/16
@@ -42,5 +46,28 @@ public class DecrementStunTest {
         WorldState updatedWorldState = decrementStun.transform(worldState);
         CharacterImpl updatedCharacter = updatedWorldState.getCharacterById("a");
         assertEquals(0, updatedCharacter.getIsStunnedForTicks());
+    }
+
+    @Test
+    public void testTransformWithoutTouchingCollisionsOrExplosions() {
+        CharacterImpl paintbotA = new CharacterImpl("a", "a", 2);
+
+        Tile[] tiles = new WorldState(3, 3).getTiles();
+        tiles[2] = new Tile(paintbotA);
+        Map<Integer, List<String>> collisions = new HashMap<>();
+        collisions.put(0, List.of("a"));
+        Map<Integer, List<String>> explosions = new HashMap<>();
+        explosions.put(0, List.of("a"));
+        WorldState worldState = new WorldState(3, 3, tiles, collisions, explosions);
+
+        DecrementStun decrementStun = new DecrementStun();
+
+        WorldState updatedWorldState = decrementStun.transform(worldState);
+        assertEquals(1, updatedWorldState.getCollisions().size());
+        assertEquals(1, updatedWorldState.getCollisions().get(0).size());
+        assertEquals("a", updatedWorldState.getCollisions().get(0).get(0));
+        assertEquals(1, updatedWorldState.getExplosions().size());
+        assertEquals(1, updatedWorldState.getExplosions().get(0).size());
+        assertEquals("a", updatedWorldState.getExplosions().get(0).get(0));
     }
 }
